10e4e23fe2409c380c46237254e030648acbf6e1
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2020-10-13 09:29:48 -0400

    ostream& explanation

---
+++ B/08-Oct13/prg.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+#include "String.h"
+using namespace sdds;
+using namespace std;
+String globalStr = "Myglobal String";
+String& whatever() {
+   return globalStr;
+}
+
+double& addToAndImpersonate(double& d) {
+   d += 2.0;
+   return d;
+}
+
+int main() {
+   double value = 5.0;
+   String name = "Fred Soley";
+   whatever().display(); // whatever become a new name for the globalStr;
+                         // using the same logic, display() of String, becomes 
+                         // a new name for the global "cout" object
+   cout << endl;
+   name.display() << setw(10) << right << 123 << endl;
+   addToAndImpersonate(value);
+   cout << value << endl;
+   addToAndImpersonate(value) += 10;
+   cout << value << endl;
+   return 0;
+}
