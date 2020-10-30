df1d852932e52bb07635b4e85d0ee64abde8c915
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2020-10-30 14:29:50 -0400

    1 PM session

--- review/session/IntArray.h
+++ review/session/IntArray.h
@@ -4,12 +4,21 @@ namespace sdds {
    class IntArray {
       int* m_data;
       unsigned int m_size;
+      void resize(unsigned int newsize);
+      void setEmpty();
    public:
+      bool isEmpty()const; 
+      IntArray();
       IntArray(unsigned int size);
+      IntArray(const IntArray& A);
+      IntArray& operator=(const IntArray& A);
       ~IntArray();
       int& element(unsigned int index);
       int& operator[](unsigned int index);
       unsigned int size()const;
+      operator bool()const;
+ //      operator unsigned int() const; // conflict with opeator bool()
+   //    operator const int* ()const; not possible because operator[] exists
    };
 }
 
--- review/session/prg.cpp
+++ review/session/prg.cpp
@@ -2,16 +2,22 @@
 #include "IntArray.h"
 using namespace std;
 using namespace sdds;
-void prn(IntArray& ar) {
-   for (unsigned int i = 0; i < ar.size(); i++) {
-      cout << ar.operator[](i) << " ";
-      //cout << ar[i] << " ";
+void prn(IntArray ar) {
+//   if(ar.operator bool()){ // if I am nuts, I can do this
+   if (ar) {
+      for (unsigned int i = 0; i < ar.size() ; i++) {
+         cout << ar.operator[](i) << " ";
+      }
+   }
+   else {
+      cout << "Empty array";
    }
    cout << endl;
 }
 int main() {
-   IntArray A(5);
+   IntArray A;
    IntArray B(10);
+   prn(A);
    A[3] = 25;
    int i;
    for (i = 0; i < 15; i++) {
@@ -19,5 +25,7 @@ int main() {
    }
    prn(A);
    prn(B);
+   A = B;
+   prn(A);
    return 0;
 }
